313535dd8b4ffd641f3bbc81f611d4ffb848aba1
S.Vinoth Kumar <srvinoth@gmail.com>
2018-04-25 20:15:19 +0400

    Update Application.java

--- src/main/java/com/todo/app/Application.java
+++ src/main/java/com/todo/app/Application.java
@@ -24,13 +24,13 @@ public class Application implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		List<Todo> todos = Arrays.asList(new Todo("Task 1", false),
-										 new Todo("Task 2", false),
-										 new Todo("Task 3", false), 
-										 new Todo("Task 4", false), 
-										 new Todo("Task 5", false),
-										 new Todo("Task 6", false), 
-										 new Todo("Task 7", true),
-										 new Todo("Task 8", true));
+					new Todo("Task 2", false),
+					new Todo("Task 3", false), 
+					new Todo("Task 4", false), 
+					new Todo("Task 5", false),
+					new Todo("Task 6", false), 
+					new Todo("Task 7", true),
+					new Todo("Task 8", true));
 		todos.forEach(todo -> repository.save(todo));
 	}
 }
